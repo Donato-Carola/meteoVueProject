c5782989122f60379801097fea18ccb0df9ae9a7
Donato-Carola <don94@live.it>
2024-05-14 11:58:15 +0200

    added function for days of the week

--- src/components/ForecastWeather.vue
+++ src/components/ForecastWeather.vue
@@ -1,62 +1,68 @@
 <template>
-    <div class="container pt-5">
-      <h2>Previsioni meteo a lungo termine per {{ city }}</h2>
-      
-        <div v-for="day in forecast" :key="day.date">
-          <h3>{{ day.date }}</h3>
+  <div class="container pt-5">
+    <h2>Previsioni meteo nei prossimi giorni</h2>
+    <h1>{{ city }}</h1>
+    <div class="container pt-3">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-4 align-self-center  " v-for="day in forecast" :key="day.date">
+          <h3>{{ getDayOfWeek(day.date) }}</h3>
           <p>Massima temperatura: {{ day.day.maxtemp_c }} °C</p>
-          <p>Minima temperatura: {{  day.day.mintemp_c }} °C</p>
+          <p>Minima temperatura: {{ day.day.mintemp_c }} °C</p>
           <!-- Aggiungi altri dettagli delle previsioni se necessario -->
         </div>
-      
-      
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from "axios";
-  
-  export default {
-    props: {
-      city: String
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    city: String,
+  },
+  data() {
+    return {
+      APIKey: "75a109a67a35426b871104513243003",
+      forecast: [],
+    };
+  },
+  methods: {
+    getDayOfWeek(date) {
+      const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      const dayIndex = new Date(date).getDay();
+      return daysOfWeek[dayIndex];
     },
-    data() {
-      return {
-        APIKey: "75a109a67a35426b871104513243003",
-        forecast: [],
-      };
+    getForecast(city) {
+      axios
+        .get(
+          `http://api.weatherapi.com/v1/forecast.json?q=${city}&key=${this.APIKey}&days=3`
+        )
+        .then((response) => {
+          console.log("Forecast Data:", response.data);
+          // Assegnamento della risposta all'oggetto previsioni a lungo termine
+          this.forecast = response.data.forecast.forecastday;
+        })
+        .catch((error) => {
+          console.error("Errore nella richiesta API forecast:", error);
+          // Puoi anche visualizzare un messaggio di errore per l'utente
+        });
     },
-    methods: {
-      getForecast(city) {
-        axios
-          .get(
-            `http://api.weatherapi.com/v1/forecast.json?q=${city}&key=${this.APIKey}&days=14`
-          )
-          .then((response) => {
-            console.log('Forecast Data:', response.data);
-            // Assegnamento della risposta all'oggetto previsioni a lungo termine
-            this.forecast = response.data.forecast.forecastday;
-          })
-          .catch((error) => {
-            console.error('Errore nella richiesta API forecast:', error);
-            // Puoi anche visualizzare un messaggio di errore per l'utente
-          });
-      },
+  },
+  watch: {
+    city(newCity, oldCity) {
+      // Aggiornamento delle previsioni quando la città cambia
+      this.getForecast(newCity);
     },
-    watch: {
-      city(newCity, oldCity) {
-        // Aggiornamento delle previsioni quando la città cambia
-        this.getForecast(newCity);
-      },
-    },
-    mounted() {
-      // Esempio di chiamata alla funzione getForecast() con la città corrente
-      this.getForecast(this.city);
-    },
-  };
-  </script>
-  
-  
-  <style>
-  /* Stili se necessario */
-  </style>
+  },
+  mounted() {
+    // Esempio di chiamata alla funzione getForecast() con la città corrente
+    this.getForecast(this.city);
+  },
+};
+</script>
+
+<style>
+/* Stili se necessario */
+</style>
